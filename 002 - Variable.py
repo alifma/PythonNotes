0ed7e049ba17b8117626e0c05a14f0b23966d707
Alif Maulana <alifmaulana26@gmail.com>
2022-12-11 20:15:53 +0700

    001-003

---
+++ 002 - Variable.py
@@ -0,0 +1,24 @@
+# Variable = Tempat menyimpan data
+
+# Assign Variable value
+# python tidak perlu melakukan definisi vairabel
+a  = 10
+x  = 5
+panjang = 5
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# variable naming
+# nilai y = 1000 : gak boleh pakai spasi
+# 10Juta         : gak boleh pakai angka di depan
+
+# Print kedua
+# Reassign variable
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+b = a
+print("Nilai b = ", b)
